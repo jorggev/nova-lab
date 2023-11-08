aceddd93d600bba665ec410b7632328162ae6bd8
Jorge David Villalba <dvillalba808@gmail.com>
2023-11-08 14:13:26 -0300

    Resolving mistakes

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 import Card from "./Card/Card";
 import Header from "./Header/Header";
 import SearchInput from "./SearchInput/SearchInput";
-import MapPreview from "./MapPreview/MapPreview";
 
-export { Card, Header, SearchInput, MapPreview};
+export { Card, Header, SearchInput};
